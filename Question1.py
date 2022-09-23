dc8d75cd7ac1c986881cf37b725373deddc451ba
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2022-09-23 18:08:47 +0100

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,38 +1,33 @@
-# imports
 from string import ascii_lowercase as alphabet
 
-def char_teleportation(strng: str) -> str:
+def char_teleportation(strng):
     prev = []
     pairs = []
-    
-    # Get pairs of letters which open portals
+
     for char_ind, char in enumerate(strng[:-1]):
         if alphabet[::-1].index(strng[char_ind + 1].lower()) == alphabet.index(char.lower()):
             pairs.append((char, strng[char_ind + 1]))
-    
-    if len(pairs) != 0: # main process: move letters around based on portal
+    if len(pairs) != 0:
         for pair in pairs:
-            if alphabet.index(pair[0].lower()) > alphabet.index(pair[1].lower()):
+            if alphabet.index(pair[0].lower()) < alphabet.index(pair[1].lower()):
                 strng = strng[:strng.index(pair[1])] + strng[strng.index(pair[1])+1:] + pair[1]
                 if strng.index(pair[0]) != 0:
-                    strng = strng[:strng.index(pair[0])-1] + strng[strng.index(pair[0]):] + strng[strng.index(pair[0]) - 1]
+                    strng = strng[strng.index(pair[1]) - 1] + strng
 
             elif alphabet.index(pair[0].lower()) > alphabet.index(pair[1].lower()):
                 strng = pair[1] + strng[:strng.index(pair[1])] + strng[strng.index(pair[1])+1:]
                 if strng.index(pair[0]) != 0:
                     strng = strng[strng.index(pair[0]) - 1] + strng[:strng.index(pair[0])-1] + strng[strng.index(pair[0]):]
-                    
-        if strng in prev: # check if you're in an infinite loop
+        if strng in prev:
             print("INDEFINITE")
             return
-            
+
         prev.append(strng)
-        char_teleportation(strng) # recursion
-        
-    elif pairs == []: # exit recursion if there are no more portals
+        char_teleportation(strng)
+    elif pairs == []:
         print(strng)
         return
-    
+
 if __name__ == "__main__":
     strng = input()
     char_teleportation(strng)
